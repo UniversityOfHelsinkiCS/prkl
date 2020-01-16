bb1b334ff2e2f1b39e447e9ad079058c15709351
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-01-16 12:31:07 +0200

    New structure

--- client/components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { images } from 'Utilities/common'
-
-export default () => (
-  <div className="footer">
-    <img src={images.toska_color} style={{ height: '100%', float: 'right' }} alt="tosca" />
-  </div>
-)
--- client/util/redux/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-
-import messages from './messageReducer'
-
-export default combineReducers({
-  messages,
-})
