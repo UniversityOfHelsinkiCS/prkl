c4be664fb0ab990b1b8f109e711cea2bf8b159a4
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-01-17 16:31:07 +0200

    Add express

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,15 @@
-console.log("Hello, TypeScript!");
+import express from "express";
+
+export const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(`server is listening on ${port}`);
+});
