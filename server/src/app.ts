a7a570925274f07e6f5d4c866181d4b2356321b8
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-01-23 11:13:33 +0200

    Add client to backend and Dockerfile

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,27 @@
+import bodyParser from "body-parser";
 import express from "express";
+import promiseRouter from "express-promise-router";
+import morgan from "morgan";
+import path from "path";
 
 export const app = express();
+const router = promiseRouter();
 const port = 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+// Logging format for morgan.
+const logFormat = process.env.NODE_ENV === "development" ? "dev" : "combined";
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`server is listening on ${port}`);
-});
+// Middleware.
+app
+  .use(bodyParser.json())
+  .use(morgan(logFormat))
+  .use(router);
+
+// Serve frontend.
+app.use(express.static("public"));
+app.get("*", (req, res) => res.sendFile(path.resolve("public", "index.html")));
+
+// Don't block ports in testing.
+if (process.env.NODE_ENV !== "test") {
+  app.listen(port);
+}
