f85ae85d32b001cc8cb5ba77cecf2eb676638f94
Kevhann <kevin.hann@helsinki.fi>
2020-01-24 11:46:12 +0200

    adding course adds it to the courses list

--- client/src/App.js
+++ client/src/App.js
@@ -12,70 +12,42 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 
 import "./App.css"
 
+createStore("coursesStore", [
+  {
+    id: 1,
+    title: "Course 1",
+    questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
+    description: "Course for epic gamers"
+  },
+  {
+    id: 2,
+    title: "Tira",
+    questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
+    description: "Helppo lasten kurssi"
+  },
+  {
+    id: 3,
+    title: "Alon",
+    questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
+    description: "Tira 2"
+  },
+  {
+    id: 4,
+    title: "JTKT",
+    questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
+    description: "Send help"
+  },
+  {
+    id: 5,
+    title: "Course 2",
+    questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
+    description: "Course for n00b gamers"
+  }
+])
+
 const App = () => {
-  createStore("coursesStore", [
-    {
-      id: 1,
-      title: "Course 1",
-      questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
-      description: "Course for epic gamers"
-    },
-    {
-      id: 2,
-      title: "Tira",
-      questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
-      description: "Helppo lasten kurssi"
-    },
-    {
-      id: 3,
-      title: "Alon",
-      questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
-      description: "Tira 2"
-    },
-    {
-      id: 4,
-      title: "JTKT",
-      questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
-      description: "Send help"
-    },
-    {
-      id: 5,
-      title: "Course 2",
-      questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
-      description: "Course for n00b gamers"
-    },
-    {
-      id: 6,
-      title: "Course 1",
-      questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
-      description: "Course for epic gamers"
-    },
-    {
-      id: 7,
-      title: "Tira",
-      questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
-      description: "Helppo lasten kurssi"
-    },
-    {
-      id: 8,
-      title: "Alon",
-      questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
-      description: "Tira 2"
-    },
-    {
-      id: 9,
-      title: "JTKT",
-      questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
-      description: "Send help"
-    },
-    {
-      id: 10,
-      title: "Course 2",
-      questions: ["ooks jonne", "juoks es", "osaatko koodaa"],
-      description: "Course for n00b gamers"
-    }
-  ])
   const [courses] = useStore("coursesStore")
+
   const courseById = id => {
     const result = courses.find(course => course.id === Number(id))
     return result
--- client/src/components/courseCreation/CourseForm.js
+++ client/src/components/courseCreation/CourseForm.js
@@ -2,98 +2,128 @@ import React, { useState } from "react"
 import { Input, Form, Button, FormGroup } from "semantic-ui-react"
 import { FormattedMessage, useIntl } from "react-intl"
 import QuestionCreationForm from "./QuestionCreationForm"
-
+import { useStore } from "react-hookstore"
 
 const CourseFrom = () => {
-    const [courseTitle, setCourseTitle] = useState("")
-    const [courseDescription, setCourseDescription] = useState("")
-    const [courseCode, setCourseCode] = useState("")
-    const [questionText, setQuestionText] = useState("")
-    const [questions, setQuestions] = useState([""])
+  const [courseTitle, setCourseTitle] = useState("")
+  const [courseDescription, setCourseDescription] = useState("")
+  const [courseCode, setCourseCode] = useState("")
+  const [questionText, setQuestionText] = useState("")
+  const [questions, setQuestions] = useState([""])
+  const [courses, setCourses] = useStore("coursesStore")
 
-    const intl = useIntl()
+  const intl = useIntl()
 
-    const handleSubmit = () => {
-        if (courseTitle && courseDescription && courseCode) {
-            const courseObject = {
-                title: courseTitle,
-                description: courseDescription,
-                code: courseCode,
-                questions: questions
+  const handleSubmit = () => {
+    if (courseTitle && courseDescription && courseCode) {
+      const courseObject = {
+        title: courseTitle,
+        description: courseDescription,
+        code: courseCode,
+        id: 100,
+        questions: questions
+      }
+
+      setCourses(courses.concat(courseObject))
+    }
+  }
+
+  const handleQuestionChange = index => (e, { value }) => {
+    const newQuestions = [...questions]
+    newQuestions[index] = value
+    setQuestions(newQuestions)
+  }
+
+  const handleAddForm = () => {
+    setQuestions([...questions, ""])
+  }
+  const handleRemoveForm = () => {
+    setQuestions(questions.slice(0, questions.length - 1))
+  }
+
+  return (
+    <div>
+      <h1>
+        <FormattedMessage id="courseCreationForm.PageTitle"></FormattedMessage>
+      </h1>
+
+      <Form>
+        <Form.Field>
+          <label>
+            {
+              <FormattedMessage id="courseCreationForm.TitleForm"></FormattedMessage>
             }
-            console.log('courseObject: ', courseObject)
-        }
-    }
+          </label>
+          <Input
+            fluid
+            type="courseTitle"
+            onChange={event => setCourseTitle(event.target.value)}
+          ></Input>
+        </Form.Field>
 
-    const handleQuestionChange = index => (e, { value }) => {
+        <Form.Field>
+          <label>
+            {
+              <FormattedMessage id="courseCreationForm.CourseDescriptionForm"></FormattedMessage>
+            }
+          </label>
+          <Input
+            fluid
+            type="courseDescription"
+            onChange={event => setCourseDescription(event.target.value)}
+          ></Input>
+        </Form.Field>
 
-        const newQuestions = [...questions]
-        newQuestions[index] = value
-        setQuestions(newQuestions)
-    }
+        <Form.Field>
+          <label>
+            {
+              <FormattedMessage id="courseCreationForm.CourseCodeForm"></FormattedMessage>
+            }
+          </label>
+          <Input
+            type="courseCode"
+            onChange={event => setCourseCode(event.target.value)}
+          ></Input>
+        </Form.Field>
 
-    const handleAddForm = () => {
-        setQuestions([...questions, ""])
-    }
-    const handleRemoveForm = () => {
-        setQuestions(questions.slice(0, questions.length - 1))
-    }
+        <Form.Group>
+          <Form.Button onClick={handleAddForm}>
+            <FormattedMessage id="questionForm.addQuestion"></FormattedMessage>
+          </Form.Button>
 
-    return (
-        <div>
+          <Form.Button onClick={handleRemoveForm}>
+            <FormattedMessage id="questionForm.removeQuestion"></FormattedMessage>
+          </Form.Button>
+        </Form.Group>
 
-            <h1><FormattedMessage id="courseCreationForm.PageTitle"></FormattedMessage></h1>
+        <Form.Group style={{ flexWrap: "wrap" }}>
+          {questions.map((q, index) => (
+            <Form.Input
+              onChange={handleQuestionChange(index)}
+              placeholder={intl.formatMessage({
+                id: "questionCreationForm.addNewQuestion"
+              })}
+              label={`Question number ${index + 1}`}
+              value={q}
+            ></Form.Input>
+          ))}
+        </Form.Group>
 
-            <Form>
-                <Form.Field >
-                    <label >{<FormattedMessage id="courseCreationForm.TitleForm"></FormattedMessage>}</label>
-                    <Input fluid type="courseTitle" onChange={event => setCourseTitle(event.target.value)}></Input>
-                </Form.Field>
-
-                <Form.Field>
-                    <label>{<FormattedMessage id="courseCreationForm.CourseDescriptionForm"></FormattedMessage>}</label>
-                    <Input fluid type="courseDescription" onChange={event => setCourseDescription(event.target.value)}></Input>
-                </Form.Field>
-
-                <Form.Field>
-                    <label>{<FormattedMessage id="courseCreationForm.CourseCodeForm"></FormattedMessage>}</label>
-                    <Input type="courseCode" onChange={event => setCourseCode(event.target.value)}></Input>
-                </Form.Field>
-
-                <Form.Group>
-                    <Form.Button onClick={handleAddForm}>
-                        <FormattedMessage id="questionForm.addQuestion"></FormattedMessage>
-                    </Form.Button>
-
-                    <Form.Button onClick={handleRemoveForm}>
-                        <FormattedMessage id="questionForm.removeQuestion"></FormattedMessage>
-                    </Form.Button>
-                </Form.Group>
-
-                <Form.Group style={{ flexWrap: "wrap" }}>
-                    {questions.map((q, index) => <Form.Input
-                        onChange={handleQuestionChange(index)}
-                        placeholder={intl.formatMessage({
-                            id: "questionCreationForm.addNewQuestion"
-                        })}
-                        label={`Question number ${index + 1}`}
-                        value={q}
-                    >
-                    </Form.Input>)}
-                </Form.Group>
-
-                {/* <Form.Field >
+        {/* <Form.Field >
                     <label>
                         <FormattedMessage id="questionCreationForm.addNewQuestion"></FormattedMessage>
                     </label>
                     <Input fluid onChange={event => setQuestionText(event.target.value)} value={questionText} />
                 </Form.Field> */}
 
-                <Form.Button onClick={handleSubmit}>{<FormattedMessage id="courseCreationForm.ConfirmButton"></FormattedMessage>}</Form.Button>
-
-            </Form>
-        </div>
-    )
+        <Form.Button onClick={handleSubmit}>
+          {
+            <FormattedMessage id="courseCreationForm.ConfirmButton"></FormattedMessage>
+          }
+        </Form.Button>
+      </Form>
+    </div>
+  )
 }
 
 export default CourseFrom
