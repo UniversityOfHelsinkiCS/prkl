b67c36443d22cfaae247a98378095af9189398ad
Kevhann <kevin.hann@helsinki.fi>
2020-02-13 16:27:20 +0200

    removed extra import

--- client/src/components/courses/Course.js
+++ client/src/components/courses/Course.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import { COURSE_BY_ID, DELETE_COURSE } from "../../GqlQueries"
-import { Form } from "semantic-ui-react"
+
 import { useHistory } from "react-router-dom"
 import { useStore } from "react-hookstore"
 
