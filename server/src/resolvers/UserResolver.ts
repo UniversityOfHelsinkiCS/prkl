89ae582ab8d71f81998389a88264bf9cb55f6f8d
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-02-13 23:24:36 +0200

    Add currentUser query

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -1,20 +1,20 @@
-import { Resolver, Query, Arg } from "type-graphql";
+import { Resolver, Query, Arg, Ctx } from "type-graphql";
 import { User } from "../entities/User";
 
 @Resolver()
 export class UserResolver {
   @Query(() => User)
-  user(@Arg("id") id: string) {
+  user(@Arg("id") id: string): Promise<User> {
     return User.findOne({ where: { id } });
   }
 
   @Query(() => [User])
-  users() {
+  users(): Promise<User[]> {
     return User.find();
   }
 
   @Query(() => User)
-  currentUser() {
-    // TODO...
+  currentUser(@Ctx() context): Promise<User> {
+    return User.findOne({ where: { shibbolethUid: context.user.shibbolethUid } });
   }
 }
