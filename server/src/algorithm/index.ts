e3c0d2a2edfc16ab913233890763f8fb498494e4
Kevhann <kevin.hann@helsinki.fi>
2020-03-06 15:53:35 +0200

    added function to create mockdata

---
+++ server/src/algorithm/index.ts
@@ -0,0 +1,7 @@
+import createData from "./mockdata";
+// kutsumalla createData(n) palauttaa n registrationia listana
+// neljä kysymystä:
+// 1. monivalinta, 4 vaihtoehtoa,
+// 2. monivalinta, 5 vaihtoehtoa,
+// 3. singlevalita, 3 vaihtoehtoa,
+// 4. singlevalita, 4 vaihtoehtoa
