5dd68eb54fa61aac700f07625833ecb8dc66ef5f
markus <markus.andersson97@gmail.com>
2020-03-18 15:05:59 +0200

    Initial version of group view using mock data added.

---
+++ client/src/components/courses/Groups.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Table, Header } from 'semantic-ui-react';
+
+
+const Groups = () => {
+
+    //Mock data
+    const groups = [[{ firstname: "Batman", lastname: "Batname", studentno: "0123", email: "batmail" }, { firstname: "Superman", lastname: "Supername", studentno: "0987", email: "supermail" }],
+    [{ firstname: "Spiderman", lastname: "Spidername", studentno: "0456", email: "spidermail" }, { firstname: "Daredevil", lastname: "Devilname", studentno: "0666", email: "devilmail" }]]
+
+    return (
+        <div>
+            <h2>Groups:</h2>
+            {groups.map((grop, index) => (
+                <div>
+                    <p></p>
+                    <Header as='h3'>
+                        <div>Group {index+1}:</div>
+                    </Header>
+                    <Table>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell>Name</Table.HeaderCell>
+                                <Table.HeaderCell>Student no.</Table.HeaderCell>
+                                <Table.HeaderCell>Email</Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Header>
+                        {grop.map(student => (
+                            <Table.Row>
+                                <Table.Cell>{student.firstname} {student.lastname}</Table.Cell>
+                                <Table.Cell>{student.studentno}</Table.Cell>
+                                <Table.Cell>{student.email}</Table.Cell>
+                            </Table.Row>
+                        ))}
+                    </Table>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Groups
