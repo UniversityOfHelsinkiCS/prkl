0a7764e6a2d74ce0e7253ee4e9dd610dc5494dcd
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-04-07 17:02:58 +0300

    Make devbar change user through backend

--- client/src/admin/DevBar.js
+++ client/src/admin/DevBar.js
@@ -1,74 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Menu } from 'semantic-ui-react';
-import { useStore } from 'react-hookstore';
+import axios from 'axios';
 
-const DevBar = () => {
-  const [user, setUser] = useStore('userStore');
-  const student = {
-    uid: '123-123',
-    firstname: 'student',
-    email: 'student@email',
-    studentNo: '123456789',
-    lastname: 'Lastname',
-    role: 1,
-    registrations: [{ course: { id: '1' } }],
-  };
-  const staff = {
-    uid: '456-456',
-    firstname: 'staffer',
-    email: 'staff@email',
-    studentNo: '987654321',
-    lastname: 'Lastname',
-    role: 2,
-    registrations: [{ course: { id: '1' } }],
-  };
-  const admin = {
-    uid: '789-789',
-    firstname: 'adminer',
-    email: 'admin@email',
-    studentNo: '133742069',
-    lastname: 'Lastname',
-    role: 3,
-    registrations: [{ course: { id: '1' } }],
-  };
-
-  const [activeItem, setActiveItem] = useState(null);
-
-  const handleItemClick = (e, { name }) => {
-    setActiveItem(name);
-    console.log('name:', name);
-    switch (name) {
-      case 'admin':
-        setUser(admin);
-        break;
-      case 'staff':
-        setUser(staff);
-        break;
-      case 'student':
-        setUser(student);
-        break;
-      default:
-        console.log('invalid user');
-    }
-  };
-  return (
-    <Menu className="mainHeader" size="massive" stackable borderless attached inverted>
-      <Menu.Item name="Home" active={activeItem === 'Home'}>
-        DEV
-      </Menu.Item>
-
-      <Menu.Item name="student" active={activeItem === 'student'} onClick={handleItemClick}>
-        Student
-      </Menu.Item>
-
-      <Menu.Item name="staff" active={activeItem === 'staff'} onClick={handleItemClick}>
-        Staff
-      </Menu.Item>
-      <Menu.Item name="admin" active={activeItem === 'admin'} onClick={handleItemClick}>
-        Admin
-      </Menu.Item>
-    </Menu>
-  );
+const switchUser = async index => {
+  await axios.post('http://localhost:3001/switchUser', { index });
+  window.location.reload();
 };
 
-export default DevBar;
+export default () => (
+  <Menu className="mainHeader" size="massive" stackable borderless attached inverted>
+    <Menu.Item>DEV</Menu.Item>
+    <Menu.Item onClick={() => switchUser(0)}>Student</Menu.Item>
+    <Menu.Item onClick={() => switchUser(1)}>Staff</Menu.Item>
+    <Menu.Item onClick={() => switchUser(2)}>Admin</Menu.Item>
+  </Menu>
+);
--- server/src/testUtils/switchUser.ts
+++ server/src/testUtils/switchUser.ts
@@ -3,6 +3,11 @@ import { headers } from "./mockUsers";
 let user = headers[0];
 
 export const switchUser = (router): void => {
+  // Make sure these routes are not mounted in production.
+  if (process.env.NODE_ENV === "production") {
+    throw new Error("Testing routes must NOT be used in production!");
+  }
+
   /**
    * Switch to the mock user defined by the field `index` which is used as index
    * to get the user from mockUsers.headers array.
