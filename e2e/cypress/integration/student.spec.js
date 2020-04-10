dc84840c53b4d6b4c6f38a296ce28da371864519
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-04-10 15:53:38 +0300

    Add tests

--- e2e/cypress/integration/student.spec.js
+++ e2e/cypress/integration/student.spec.js
@@ -4,6 +4,7 @@ describe('Student', () => {
   beforeEach(() => {
     cy.resetDatabase();
     cy.switchToStudent();
+    cy.fixture('courses').as('courses');
   });
 
   it('Can see their personal info.', () => {
@@ -16,21 +17,42 @@ describe('Student', () => {
     cy.contains('Email:');
   });
 
-  it('Can see the course listing', () => {
+  it('Can see the course listing', function () {
     cy.createCourse(0);
     cy.visit('/');
-    cy.contains('Test Course 0');
+    cy.contains(this.courses[0].title);
   });
 
-  it.only('Can enrol on a course', () => {
+  it('Can enrol on a course', function () {
     cy.createCourse(0);
     cy.visit('/');
-    cy.contains('Test Course 0').click();
+    cy.contains(this.courses[0].title).click();
+
+    cy.get('[data-cy="question-0"]').type('Answer');
+    cy.get('[data-cy="question-1"]').click();
+    cy.contains('Second choice').click();
+
+    cy.get('[data-cy="question-2"]').click();
+
+    cy.get('[data-cy="question-2"]').contains('First choice').then(item => {
+      item.click();
+    });
 
     cy.get('[data-cy="toc-checkbox"]').click();
     cy.get('[data-cy="submit-button"]').click();
     cy.get('[data-cy="confirm-button"]').click();
 
     cy.contains('Great success!');
+  });
+
+  it('Can not enrol with answers missing', function () {
+    cy.createCourse(0);
+    cy.visit('/');
+    cy.contains(this.courses[0].title).click();
+
+    cy.get('[data-cy="submit-button"]').click();
+
+    cy.get('[data-cy="confirm-button"]').should('not.exist');
+    cy.contains('Please answer all questions!');
   });
 });
