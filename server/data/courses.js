04a7fa017ff9f75d76445a7178307e8d6d9a6a53
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-04-15 22:00:12 +0300

    Add some mock data

--- server/data/courses.js
+++ server/data/courses.js
@@ -8,4 +8,50 @@ module.exports = [
     maxGroupSize: 10,
     minGroupSize: 5,
   },
+  {
+    id: "be2a695a-c3a4-4f25-b0b5-ac7d4854a5b1",
+    title: "Test Course 2",
+    deadline: "2100-12-31",
+    code: "TC02",
+    description: "Description for a test course.",
+    maxGroupSize: 10,
+    minGroupSize: 5,
+    questions: [
+      {
+        questionType: "singleChoice",
+        content: "What is your single choice?",
+        order: 0,
+        questionChoices: [
+          {
+            order: 0,
+            content: "First choice",
+          },
+          {
+            order: 1,
+            content: "Second choice",
+          },
+        ],
+      },
+      {
+        questionType: "multipleChoice",
+        content: "What are your many choices?",
+        order: 1,
+        questionChoices: [
+          {
+            order: 0,
+            content: "First or many choices",
+          },
+          {
+            order: 1,
+            content: "Second of many choices",
+          },
+        ],
+      },
+      {
+        questionType: "freeForm",
+        content: "How do you be?",
+        order: 2,
+      },
+    ],
+  },
 ];
