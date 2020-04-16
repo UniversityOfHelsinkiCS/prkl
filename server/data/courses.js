dbe8569241e18d3d5773ee4bdc1ef461c846a839
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-04-16 20:01:07 +0300

    Test data deps in CI

--- server/data/courses.js
+++ server/data/courses.js
@@ -1,7 +1,9 @@
+const users = require("./users");
+
 module.exports = [
   {
     id: "af4f17f3-9e1a-4724-aa5c-e8c5042909ec",
-    title: "Test Course 1",
+    title: "Test Course 1" + users[0].firstname,
     deadline: "2100-12-31",
     code: "TC01",
     description: "Description for a test course.",
