86f031747be94b503d10df0a45358dab059a9dbb
Riikka Räty <48516441+riiraty@users.noreply.github.com>
2020-09-18 15:51:21 +0300

    Test staffcontrols, group tests by feature.

--- e2e/cypress/integration/admin.spec.js
+++ e2e/cypress/integration/admin.spec.js
@@ -7,8 +7,22 @@ describe('Admin', () => {
     cy.switchToAdmin();
   });
 
-  it('Can see an unpublished course', () => {
-    cy.visit('/courses');
-    cy.contains(courses[2].title);
+  describe('course listing', () => {
+    it('Can see an unpublished course', () => {
+      cy.visit('/courses');
+      cy.contains(courses[2].title);
+    });
+
+    it('Can see staffcontrols', () => {
+      cy.visit('/courses');
+      cy.get('[data-cy="checkbox-staff-controls"]').should('exist')
+    });
+  
+    it('Can toggle to see only own courses', () => {
+      cy.visit('/courses');
+      cy.get('[data-cy="checkbox-staff-controls"]').last().click();
+      cy.contains(courses[0].title).should('not.exist');
+    });
   });
+
 });
--- e2e/cypress/integration/student.spec.js
+++ e2e/cypress/integration/student.spec.js
@@ -9,146 +9,162 @@ describe('Student', () => {
     cy.fixture('courses').as('courses');
   });
 
-  it('Can see their personal info.', () => {
-    cy.visit('/');
-    cy.contains('Personal info').click();
-    cy.url().should('include', '/user');
-
-    cy.contains('Name:');
-    cy.contains('Student number:');
-    cy.contains('Email:');
-  });
-
-  it('Can see the course listing', () => {
-    cy.visit('/');
-    cy.contains(courses[0].title);
-  });
-
-  it('Can not see an unpublished course', () => {
-    cy.visit('/courses');
-    cy.contains(courses[2].title).not();
-  });
-
-  it('Can enrol on a course', () => {
-    const course = courses[1];
-    cy.visit('/');
-    cy.contains(courses[1].title).click();
-
-    cy.get('[data-cy="question-0"]').click();
-    const answers = [course.questions[0].questionChoices[1].content];
-    cy.contains(answers[0]).click();
-
-    cy.get('[data-cy="question-1"]').click();
-    answers.push(course.questions[1].questionChoices[1].content);
-    cy.get('[data-cy="question-1"]').contains(answers[1]).then((item) => {
-      item.click();
+  describe('access', () => {
+    it('Can not see staff and admin views', () => {
+      cy.visit('/addcourse');
+      cy.contains('You do not have the required roles');
+  
+      cy.visit('/usermanagement');
+      cy.contains('You do not have the required roles');
     });
 
-    answers.push('My cool answer');
-    cy.get('[data-cy="question-2"]').type(answers[2]);
+    it('Cannot create a course', () => {
+      cy.createCourse(0, 0).then((resp) => {
+        expect(resp.status).to.eq(500);
+      });
+    });
 
-    cy.get('[data-cy="toc-checkbox"]').click();
-    cy.get('[data-cy="submit-button"]').click();
-    cy.get('[data-cy="confirm-button"]').click();
-
-    cy.contains('Great success!');
-
-    // Admin-role check for correct answers.
-    cy.switchToAdmin();
-    cy.visit(`/course/${course.id}`);
-    for (const answer of answers) {
-      cy.contains(answer);
-    }
   });
 
-  it('Can not enrol with answers missing', () => {
-    cy.visit('/');
-    cy.contains(courses[1].title).click();
+  describe('course listing', () => {
+    it('Can see the course listing', () => {
+      cy.visit('/');
+      cy.contains(courses[0].title);
+    });
+    
+    it('Can not see an unpublished course', () => {
+      cy.visit('/');
+      cy.contains(courses[2].title).should('not.exist');
+    });
 
-    cy.get('[data-cy="submit-button"]').click();
+    it('Can not see past courses', () => {
+      cy.switchToStaff();
+      cy.get('[data-cy="menu-item-add-course"]').click();
+  
+      cy.get('[data-cy="course-title-input"]').type('Past Course');
+      cy.get('[data-cy="course-code-input"]').type('CYP999');
+      cy.get('[data-cy="course-deadline-input"]').type('2019-12-12');
+      cy.get('[data-cy="course-description-input"]').type('Description for test course.');
+  
+      cy.get('[data-cy="create-course-submit"]').click();
+  
+      cy.switchToStudent();
+      cy.visit('/courses');
+      cy.get('[data-cy="loader"]').should('not.exist');
+      cy.contains('Past Course').should('not.exist');
+    });
+  
+    it('Can not see deleted courses', () => {
+      cy.switchToAdmin();
+      cy.contains(courses[0].title).click();
+      cy.get('[data-cy="delete-course-button"]').click();
+  
+      cy.switchToStudent();
+      cy.visit('/courses');
+      cy.get('[data-cy="loader"]').should('not.exist');
+      cy.contains(courses[0].title).should('not.exist');
+    });
 
-    cy.get('[data-cy="confirm-button"]').should('not.exist');
-    cy.contains('Please answer all questions!');
+    it('Can not see staffcontrols', () => {
+      cy.visit('/courses');
+      cy.get('[data-cy="checkbox-staff-controls"]').should('not.exist');
+    });
+  
   });
 
-  it('Can not enrol without accepting the privacy terms', () => {
-    cy.visit('/');
-    cy.contains(courses[0].title).click();
+  describe('personal info', () => {
+    it('Can see their personal info.', () => {
+      cy.visit('/');
+      cy.contains('Personal info').click();
+      cy.url().should('include', '/user');
+  
+      cy.contains('Name:');
+      cy.contains('Student number:');
+      cy.contains('Email:');
+    });
 
-    // Cycle the checkbox on once to account for a past validation bug.
-    cy.get('[data-cy="toc-checkbox"]').click();
-    cy.get('[data-cy="toc-checkbox"]').click();
-    cy.get('[data-cy="submit-button"]').click();
-
-    cy.get('[data-cy="confirm-button"]').should('not.exist');
-    cy.contains('Please answer all questions!');
-  });
-
-  it('Can not enrol twice on the same course', () => {
-    cy.visit('/');
-    cy.contains(courses[0].title).click();
-
-    cy.get('[data-cy="toc-checkbox"]').click();
-    cy.get('[data-cy="submit-button"]').click();
-    cy.get('[data-cy="confirm-button"]').click();
-
-    cy.visit('/');
-    cy.contains(courses[0].title).click();
-    cy.contains('Already registered!');
-    cy.get('[data-cy="submit-button"]').should('not.exist');
-  });
-
-  it('Can see which courses they have enrolled to', () => {
-    cy.visit('/');
-    cy.contains(courses[0].title).click();
-
-    cy.get('[data-cy="toc-checkbox"]').click();
-    cy.get('[data-cy="submit-button"]').click();
-    cy.get('[data-cy="confirm-button"]').click();
-
-    cy.visit('/user');
-    cy.contains(courses[0].title);
-  });
-
-  it('Can not see staff and admin views', () => {
-    cy.visit('/addcourse');
-    cy.contains('You do not have the required roles');
-
-    cy.visit('/usermanagement');
-    cy.contains('You do not have the required roles');
-  });
-
-  it('Can not see past courses', () => {
-    cy.switchToStaff();
-    cy.get('[data-cy="menu-item-add-course"]').click();
-
-    cy.get('[data-cy="course-title-input"]').type('Past Course');
-    cy.get('[data-cy="course-code-input"]').type('CYP999');
-    cy.get('[data-cy="course-deadline-input"]').type('2019-12-12');
-    cy.get('[data-cy="course-description-input"]').type('Description for test course.');
-
-    cy.get('[data-cy="create-course-submit"]').click();
-
-    cy.switchToStudent();
-    cy.visit('/courses');
-    cy.get('[data-cy="loader"]').should('not.exist');
-    cy.contains('Past Course').should('not.exist');
-  });
-
-  it('Can not see deleted courses', () => {
-    cy.switchToAdmin();
-    cy.contains(courses[0].title).click();
-    cy.get('[data-cy="delete-course-button"]').click();
-
-    cy.switchToStudent();
-    cy.visit('/courses');
-    cy.get('[data-cy="loader"]').should('not.exist');
-    cy.contains(courses[0].title).should('not.exist');
-  });
-
-  it('Cannot create a course', () => {
-    cy.createCourse(0, 0).then((resp) => {
-      expect(resp.status).to.eq(500);
+    it('Can see which courses they have enrolled to', () => {
+      cy.visit('/');
+      cy.contains(courses[0].title).click();
+  
+      cy.get('[data-cy="toc-checkbox"]').click();
+      cy.get('[data-cy="submit-button"]').click();
+      cy.get('[data-cy="confirm-button"]').click();
+  
+      cy.visit('/user');
+      cy.contains(courses[0].title);
     });
   });
+
+  describe('course enrolment', () => {
+    it('Can enrol on a course', () => {
+      const course = courses[1];
+      cy.visit('/');
+      cy.contains(courses[1].title).click();
+  
+      cy.get('[data-cy="question-0"]').click();
+      const answers = [course.questions[0].questionChoices[1].content];
+      cy.contains(answers[0]).click();
+  
+      cy.get('[data-cy="question-1"]').click();
+      answers.push(course.questions[1].questionChoices[1].content);
+      cy.get('[data-cy="question-1"]').contains(answers[1]).then((item) => {
+        item.click();
+      });
+  
+      answers.push('My cool answer');
+      cy.get('[data-cy="question-2"]').type(answers[2]);
+  
+      cy.get('[data-cy="toc-checkbox"]').click();
+      cy.get('[data-cy="submit-button"]').click();
+      cy.get('[data-cy="confirm-button"]').click();
+  
+      cy.contains('Great success!');
+  
+      // Admin-role check for correct answers.
+      cy.switchToAdmin();
+      cy.visit(`/course/${course.id}`);
+      for (const answer of answers) {
+        cy.contains(answer);
+      }
+    });
+  
+    it('Can not enrol with answers missing', () => {
+      cy.visit('/');
+      cy.contains(courses[1].title).click();
+  
+      cy.get('[data-cy="submit-button"]').click();
+  
+      cy.get('[data-cy="confirm-button"]').should('not.exist');
+      cy.contains('Please answer all questions!');
+    });
+  
+    it('Can not enrol without accepting the privacy terms', () => {
+      cy.visit('/');
+      cy.contains(courses[0].title).click();
+  
+      // Cycle the checkbox on once to account for a past validation bug.
+      cy.get('[data-cy="toc-checkbox"]').click();
+      cy.get('[data-cy="toc-checkbox"]').click();
+      cy.get('[data-cy="submit-button"]').click();
+  
+      cy.get('[data-cy="confirm-button"]').should('not.exist');
+      cy.contains('Please answer all questions!');
+    });
+  
+    it('Can not enrol twice on the same course', () => {
+      cy.visit('/');
+      cy.contains(courses[0].title).click();
+  
+      cy.get('[data-cy="toc-checkbox"]').click();
+      cy.get('[data-cy="submit-button"]').click();
+      cy.get('[data-cy="confirm-button"]').click();
+  
+      cy.visit('/');
+      cy.contains(courses[0].title).click();
+      cy.contains('Already registered!');
+      cy.get('[data-cy="submit-button"]').should('not.exist');
+    });
+  });
+
 });
