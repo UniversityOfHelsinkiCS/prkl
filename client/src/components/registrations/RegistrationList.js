1764c54228195a61c64e7442dbea944ea16f7644
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-11-04 00:58:29 +0200

    Refactor: - small changes to make code prettier - move a query from App to another component to avoid making it unnecessarily

--- client/src/components/registrations/RegistrationList.js
+++ client/src/components/registrations/RegistrationList.js
@@ -7,11 +7,18 @@ import roles from '../../util/userRoles';
 export default ({course, registrations, setRegistrations }) => {
   const [user] = useStore('userStore');
 
+  const hasAccess = () => {
+    if (user.role === roles.ADMIN_ROLE) {
+      return true;
+    }
+    return user.role === roles.STAFF_ROLE && course.teachers.find(t => t.id === user.id) !== undefined;
+  }
+  
   return (
     <div>
       <p />
       <div>
-        {course.questions && registrations && (user.role === roles.ADMIN_ROLE || (user.role === roles.STAFF_ROLE && course.teachers.find(t => t.id === user.id) !== undefined)) ? (
+        {course.questions && registrations && hasAccess ? (
           <div>
             <CourseRegistrations course={course} registrations={registrations} setRegistrations={setRegistrations} />
           </div>
