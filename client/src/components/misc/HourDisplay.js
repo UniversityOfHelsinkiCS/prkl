153b9f61ad246fc238defc9d9888eb3d75460fdb
Riikka Räty <48516441+riiraty@users.noreply.github.com>
2020-11-12 14:58:23 +0200

    Clearer distinction on suitable group times display

--- client/src/components/misc/HourDisplay.js
+++ client/src/components/misc/HourDisplay.js
@@ -12,12 +12,20 @@ const HourDisplay = ({ header, times, students, groupId }) => {
   // get the transpose of times
   const timesTranspose = times[0].map((col, i) => times.map(row => row[i]));
 
-  const hsl_col_perc = (percent, start, end) => {
-    const b = (end - start) * percent;
-    const c = b + start;
+  const hsl_col_code = (timeSlot) => {
+    const ratio = timeSlot / students;
+    let code = 0; // red
+
+    if (ratio == 1){
+      code = 120; // green
+    } else if (ratio >= 0.5) {
+      code = 60; // yellow
+    } else if (ratio > 0) {
+      code = 30; // orange
+    }
 
     // Return a CSS HSL string
-    return 'hsl(' + c + ', 100%, 50%)';
+    return `hsl(${code}, 100%, 70%)`
   };
 
   return (
@@ -49,7 +57,7 @@ const HourDisplay = ({ header, times, students, groupId }) => {
             {hours.map((timeSlot, timeSlotIndex) => (
               <Table.Cell
                 key={`${timeSlotIndex}-${groupId}-${index}`}
-                style={{ padding: 0, backgroundColor: hsl_col_perc(timeSlot / students, 0, 120) }}
+                style={{ padding: 0, backgroundColor: hsl_col_code(timeSlot) }}
               >
                 {timeSlot}
               </Table.Cell>
