31fca4a4c0f2fa27f4f6de88c5d24d0f5c8f1503
Koppari <robin.stachon@helsinki.fi>
2021-01-27 15:31:02 +0200

    added truncated descriptions to course listing

--- client/src/components/courses/CourseList.js
+++ client/src/components/courses/CourseList.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { useIntl } from 'react-intl';
 import CourseTag from './CourseTag';
 
-export default ({ courses, user }) => {
+export default ({ courses, user, registrations }) => {
   const intl = useIntl();
 
   return (
@@ -18,21 +18,36 @@ export default ({ courses, user }) => {
             fluid
             as={Link}
             to={`/course/${course.id}`}
-            className={new Date(course.deadline) < new Date() ? 'course-past' : null}
+            className={new Date(course.deadline) < new Date() ? 'course-past' : null}       
           >
             <Card.Content>
               <Card.Header>{course.code} - {course.title}</Card.Header>
+
               <Card.Description>{intl.formatMessage({
                 id: 'courses.deadline',
                 })} {intl.formatDate(course.deadline)}
               </Card.Description>
+
+              {/* move style to App.css later*/}
+              <Card.Description 
+                style={{
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                  width: '50%'
+                }}
+              >
+                {course.description}
+              </Card.Description>
+              
               <Card.Content>
                 <CourseTag course={course} user={user} />
               </Card.Content>
+             
             </Card.Content>  
           </Card>
         ))}
       </div>
-    </Card.Group>
+    </Card.Group>    
   );
 };
