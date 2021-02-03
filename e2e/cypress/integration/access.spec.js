569e34a213d187436d3fa4b9926cfefc04bd2e05
Koppari <robin.stachon@helsinki.fi>
2021-02-03 16:03:52 +0200

    updated tests for teachers list

--- e2e/cypress/integration/access.spec.js
+++ e2e/cypress/integration/access.spec.js
@@ -33,16 +33,17 @@ describe('User access and content', () => {
     cy.wait(500);
 
     //course descriptions
-    cy.get('[data-cy="TC01"]').within(() => {
+    cy.get(`[data-cy="${courses[0].code}"]`).within(() => {
       cy.contains(courses[0].description);
     });
 
     //course teachers
     cy.get(`[data-cy="${courses[0].code}"]`).within(() => {
+      cy.contains('Teachers').click()
       cy.contains(courses[0].teachers[0].firstname);
       cy.contains(courses[0].teachers[0].lastname);
       cy.contains(courses[0].teachers[0].email);
-    })
+    }).focus().blur();
 
     //cannot see staff controls
     cy.get('[data-cy="checkbox-staff-controls"]').should('not.exist');
