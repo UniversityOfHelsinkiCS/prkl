774fd4703fe9378b71de5d20a3de85c297d803c2
Benjamin Blinnikka <benjamin.blinnikka@hotmail.com>
2021-02-10 18:40:13 +0200

    Issue 188:
    - Initial combined algo.
    - ESLint fixes here and there

--- server/src/algorithm/algorithm.ts
+++ server/src/algorithm/algorithm.ts
@@ -5,90 +5,106 @@ import * as _ from "lodash";
 import evaluateGroupByMultipleChoice from "./evaluators/evaluateByMultipleChoice";
 import evaluateGroupByWorkingHours from "./evaluators/evaluateByWorkingHours";
 
-export type Algorithm = (targetGroupSize: number, registrations: Registration[]) => GroupInput[]
+export type Algorithm = (targetGroupSize: number, registrations: Registration[]) => GroupInput[];
 
-export type Evaluator = (group: Group) => number
+export type Evaluator = (group: Group) => number;
 
-export type Group = Registration[]
+export type Group = Registration[];
 
-export type Grouping = Group[]
+export type Grouping = Group[];
 
-const sum = (arr: number[]) => arr.reduce((sum, val) => sum + val, 0)
+const sum = (arr: number[]) => arr.reduce((sum, val) => sum + val, 0);
 
-
-// PSEUDOCODE
+/*
+const EVALUATORS = [
+  [evaluateGroupByMultipleChoice, 0.5],
+  [evaluateGroupByWorkingHours, 0.5],
+];
+ */
 
 const EVALUATORS = [
-    [evaluateGroupByMultipleChoice, evaluateGroupByWorkingHours]
-]
+  {
+    evaluate: evaluateGroupByWorkingHours,
+    multiply: 0.5,
+  },
+  {
+    evaluate: evaluateGroupByMultipleChoice,
+    multiply: 0.5,
+  },
+];
 
-const ITERATIONS = 200
+const ITERATIONS = 100;
 
+// Goes through both evaluators with multipliers
 const scoreGrouping = (grouping: Grouping) => {
-    return sum(grouping.map(evaluateGroupByWorkingHours))
-}
+  return sum(grouping.map(group =>
+            sum(EVALUATORS.map(evaluator =>
+                evaluator.evaluate(group) * evaluator.multiply)
+            ))
+         );
+};
 
-// TODO: refactoring
+// Only multiple-choice-evaluating
 const scoreGroupingByChoices = (grouping: Grouping) => {
-    return sum(grouping.map(evaluateGroupByMultipleChoice))
-}
-
-// TODO: refactoring
-export const formGroupsByMultiple: Algorithm = (targetGroupSize: number, registrations: Registration[]): GroupInput[] => {
-    let grouping: Group[] = createRandomGrouping(targetGroupSize, registrations)
-    let score = scoreGroupingByChoices(grouping)
-
-    for (let i = 0; i < ITERATIONS; i++) {
-        const newGrouping = mutateGrouping(grouping)
-
-        if (scoreGroupingByChoices(newGrouping) > score) {
-            score = scoreGroupingByChoices(newGrouping)
-            grouping = newGrouping
-        }
-    }
-
-    console.log("Final grouping score: " + score)
-
-    return grouping.map(group => ({userIds: group.map(registration => registration.student.id)}) as GroupInput)
-}
-
-export const formGroups: Algorithm = (targetGroupSize: number, registrations: Registration[]): GroupInput[] => {
-    let grouping: Group[] = createRandomGrouping(targetGroupSize, registrations)
-    let score = scoreGrouping(grouping)
-
-    for (let i = 0; i < ITERATIONS; i++) {
-        const newGrouping = mutateGrouping(grouping)
-
-        if (scoreGrouping(newGrouping) > score) {
-            score = scoreGrouping(newGrouping)
-            grouping = newGrouping
-        }
-    }
-
-    console.log("Final grouping score: " + score)
-
-    return grouping.map(group => ({userIds: group.map(registration => registration.student.id)}) as GroupInput)
-}
+  return sum(grouping.map(evaluateGroupByMultipleChoice));
+};
 
 const createRandomGrouping = (targetGroupSize: number, registrations: Registration[]) => {
-    const shuffled = _.shuffle(registrations)
-    const groups = _.chunk(shuffled, targetGroupSize)
-    return groups
-}
+  const shuffled = _.shuffle(registrations);
+  const groups = _.chunk(shuffled, targetGroupSize);
+  return groups;
+};
 
 const mutateGrouping = (grouping: Grouping) => {
-    if (grouping.length < 2) {
-        return grouping
+  if (grouping.length < 2) {
+    return grouping;
+  }
+
+  const groupsToSwapFrom = _.sampleSize(grouping, 2);
+  const rest = _.without(grouping, ...groupsToSwapFrom);
+
+  const group1User = _.sample(groupsToSwapFrom[0]);
+  const group1Rest = _.without(groupsToSwapFrom[0], group1User);
+
+  const group2User = _.sample(groupsToSwapFrom[1]);
+  const group2Rest = _.without(groupsToSwapFrom[1], group2User);
+
+  return [...rest, group2Rest.concat(group1User), group1Rest.concat(group2User)];
+};
+
+// Uses both evaluators.
+export const combinedAlgo: Algorithm = (targetGroupSize: number, registrations: Registration[]): GroupInput[] => {
+  let grouping: Group[] = createRandomGrouping(targetGroupSize, registrations);
+  let score = scoreGrouping(grouping);
+
+  for (let i = 0; i < ITERATIONS; i++) {
+    const newGrouping = mutateGrouping(grouping);
+    const newScore = scoreGrouping(newGrouping);
+
+    if (newScore > score) {
+      score = newScore;
+      grouping = newGrouping;
     }
+  }
 
-    const groupsToSwapFrom = _.sampleSize(grouping, 2)
-    const rest = _.without(grouping, ...groupsToSwapFrom)
+  console.log("Final grouping score: ", score);
+  return grouping.map(group => ({ userIds: group.map(registration => registration.student.id) } as GroupInput));
+};
 
-    const group1User = _.sample(groupsToSwapFrom[0])
-    const group1Rest = _.without(groupsToSwapFrom[0], group1User)
+// Based solely on given single- / multiple-choice-answers
+export const formGroupsByMultiple: Algorithm = (targetGroupSize: number, registrations: Registration[]): GroupInput[] => {
+  let grouping: Group[] = createRandomGrouping(targetGroupSize, registrations);
+  let score = scoreGroupingByChoices(grouping);
 
-    const group2User = _.sample(groupsToSwapFrom[1])
-    const group2Rest = _.without(groupsToSwapFrom[1], group2User)
+  for (let i = 0; i < ITERATIONS; i++) {
+    const newGrouping = mutateGrouping(grouping);
 
-    return [...rest, group2Rest.concat(group1User), group1Rest.concat(group2User)]
-}
+    if (scoreGroupingByChoices(newGrouping) > score) {
+      score = scoreGroupingByChoices(newGrouping);
+      grouping = newGrouping;
+    }
+  }
+
+  console.log("Final grouping score: " + score);
+  return grouping.map(group => ({ userIds: group.map(registration => registration.student.id) } as GroupInput));
+};
