1402a4abb87d69d858318a8b70c9f680589afdba
Koppari <robin.stachon@helsinki.fi>
2021-02-19 12:57:08 +0200

    Issue #154 	- added better view for groupless students 	- bugs: 		- when refreshing localhost:3000, all students are shown as groupless 		- when a student is removed from a group and cancel is pressed, student is still shown as groupless

--- client/src/components/groups/GrouplessStudents.js
+++ client/src/components/groups/GrouplessStudents.js
@@ -1,1 +1,70 @@
-// make component for groupless students here, use Groups.js segments as base
+import React, { useState, useEffect } from 'react';
+import { Table, Segment, Label } from 'semantic-ui-react';
+import { FormattedMessage, useIntl } from 'react-intl';
+
+export default ({
+    grouplessStudents
+}) => {
+
+    return (
+        <div>
+          <Segment>
+
+            <Label
+              color="grey"
+              size="large"
+              attached="top"
+              data-cy="group-name-label"
+            >
+                {'Groupless students'}
+            </Label>
+
+            <Table singleLine fixed>  
+              <Table.Header>
+                <Table.Row> 
+
+                  <Table.HeaderCell>
+                    <FormattedMessage id="groups.name" />
+                  </Table.HeaderCell>
+                      
+                  <Table.HeaderCell>
+                    <FormattedMessage id="groups.studentNumber" />
+                  </Table.HeaderCell>
+
+                  <Table.HeaderCell>
+                    <FormattedMessage id="groups.email" />
+                  </Table.HeaderCell>
+
+                  <Table.HeaderCell />
+
+                </Table.Row> 
+              </Table.Header>
+
+              <Table.Body>
+                {grouplessStudents.map(student => {
+                  return (
+                    <Table.Row>
+
+                      <Table.Cell>
+                        {`${student.firstname} ${student.lastname}`}
+                      </Table.Cell>
+
+                      <Table.Cell>
+                        {student.studentNo}
+                      </Table.Cell>
+
+                      <Table.Cell>
+                        {student.email}
+                      </Table.Cell>
+
+                    </Table.Row>
+                  )
+                })}
+              </Table.Body>
+            </Table>
+
+          </Segment>
+          <br />
+        </div>
+    )
+}
