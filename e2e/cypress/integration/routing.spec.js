d0eaa6b372d2b4592bd78455f1d8bf0b3a8b7abf
Koppari <robin.stachon@helsinki.fi>
2021-03-04 16:31:34 +0200

    Fixed Cypress for CI

--- e2e/cypress/integration/routing.spec.js
+++ e2e/cypress/integration/routing.spec.js
@@ -12,26 +12,26 @@ describe('Routing', () => {
       cy.switchToAdmin();
   
       cy.contains(courses[0].title).click();
-      cy.url().should('eq', `http://localhost:3000/course/${courses[0].id}`)
+      cy.url().should('include', `/course/${courses[0].id}`)
   
       // edit page
       cy.get('[data-cy="edit-course-button"]').click();
-      cy.url().should('eq', `http://localhost:3000/course/${courses[0].id}/edit`)
+      cy.url().should('include', `/course/${courses[0].id}/edit`)
       cy.get('[data-cy="create-course-cancel"]').click();
       cy.get('[data-cy="confirmation-button-confirm"]').click();
-      cy.url().should('eq', `http://localhost:3000/course/${courses[0].id}`)
+      cy.url().should('include', `/course/${courses[0].id}`)
   
       // groups page
       cy.get('[data-cy="manage-groups-button"]').click();
-      cy.url().should('eq', `http://localhost:3000/course/${courses[0].id}/groups`)
+      cy.url().should('include', `/course/${courses[0].id}/groups`)
       cy.get('[data-cy="back-to-info-from-groups-button"]').click();
-      cy.url().should('eq', `http://localhost:3000/course/${courses[0].id}`)
+      cy.url().should('include', `/course/${courses[0].id}`)
   
       // registrations page
       cy.get('[data-cy="show-registrations-button"]').click();
-      cy.url().should('eq', `http://localhost:3000/course/${courses[0].id}/registrations`)
+      cy.url().should('include', `/course/${courses[0].id}/registrations`)
       cy.get('[data-cy="back-to-info-from-groups-button"]').click();
-      cy.url().should('eq', `http://localhost:3000/course/${courses[0].id}`)
+      cy.url().should('include', `/course/${courses[0].id}`)
     });
 
     // make one for staff and students - they shouldnt see anything/be unable to access urls meant only for admins
