6ad93e33e6599c61e6c25cb3727099c11ebd8968
D3lux3 <38981163+D3lux3@users.noreply.github.com>
2022-01-27 18:22:58 +0200

    Testing trunk pushing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -39,7 +39,7 @@ export default () => {
     <AppBar position="static" color="transparent">
       <Toolbar>
         <Typography component={Link} to="/" variant="h5" className={classes.title}>
-          Assembler
+          AssemblerTesti
         </Typography>
 
         <Grid container>
