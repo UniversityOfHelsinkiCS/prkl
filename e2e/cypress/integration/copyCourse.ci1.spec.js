93da907e92dd27bc5d41084e61c5d829d13ce68f
Erika Horros-Wallin <erika.horros@gmail.com>
2022-03-23 19:32:03 +0200

    Small fixes to copycoursetest and add fetchpolicy to copy

--- e2e/cypress/integration/copyCourse.ci1.spec.js
+++ e2e/cypress/integration/copyCourse.ci1.spec.js
@@ -42,6 +42,7 @@ describe('Copying old course data when add course', () => {
     cy.get('[data-cy="course-title-input"] input').should('have.value', 'Course from Cypress');
     cy.get('[data-cy="course-title-input"]').clear().type('Second Course from Cypress');
     cy.get('[data-cy="course-deadline-input"]').type('2100-12-12');
+    cy.get('[data-cy="course-code-input"]').clear().type('CYP969');
     //Create question 2
     cy.get('[data-cy="add-question-button"]').click();
     cy.get('[data-cy="question-title"]').last().type('Greetings from second course');
@@ -50,30 +51,11 @@ describe('Copying old course data when add course', () => {
     cy.get('[data-cy="create-course-submit"]').click();
     cy.get('[data-cy="confirmation-button-confirm"]').click();
 
-    cy.visit('/addcourse');
-    //Create third course from seconf
-    cy.get('[data-cy="course-code-input"]').type('CYP999');
-    cy.get('[data-cy="search-code-button"]').click();
-    //Check if copy work
-    cy.get('[data-cy="course-title-input"] input').should('have.value', 'Second Course from Cypress');
-    cy.get('[data-cy="course-code-input"]').clear().type('CYP969');
-    cy.get('[data-cy="course-title-input"]').clear().type('Third Course from Cypress');
-    //Create question 2
-    cy.get('[data-cy="add-question-button"]').click();
-    cy.get('[data-cy="question-title"]').last().type('Greetings from third course');
-    cy.get('[data-cy="question-type-freeform"]').last().click();
-
-    cy.get('[data-cy="course-deadline-input"]').type('2100-12-12');
-    cy.wait(5000);
-    cy.get('[data-cy="create-course-submit"]').click();
-    cy.get('[data-cy="confirmation-button-confirm"]').click();
-
     cy.get('[data-cy="CYP969"]').first().click();
-    cy.contains('Third Course from Cypress');
     //Check if all copy work
     cy.contains('Greetings from first course');
     cy.contains('Greetings from second course');
-    cy.contains('Greetings from third course');
+
     });
 
 });
