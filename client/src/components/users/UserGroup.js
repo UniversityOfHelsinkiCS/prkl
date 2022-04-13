5b1b866d6e5b68f7d8e8be57f03d1011bb406d4c
Erika Horros-Wallin <erika.horros@gmail.com>
2022-04-13 12:35:15 +0300

    Add new layout for personal info groups

--- client/src/components/users/UserGroup.js
+++ client/src/components/users/UserGroup.js
@@ -1,23 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useQuery } from '@apollo/client';
-import { Box, makeStyles, Paper, Typography } from '@material-ui/core';
-import { grey } from '@material-ui/core/colors';
 import { GROUP_TIMES } from '../../GqlQueries';
 import { timeParse } from '../../util/functions';
 import UserGroupItem from './UserGroupItem';
-
-const useStyles = makeStyles({
-  messageBox: {
-    position: 'relative',
-    left: -15,
-    backgroundColor: grey[300],
-    marginTop: 50,
-  },
-  message: {
-    wordWrap: 'break-word',
-  },
-});
 
 export default ({ user, course }) => {
   const [groupTimes, setGroupTimes] = useState(undefined);
@@ -53,34 +39,18 @@ export default ({ user, course }) => {
 
   const userIsRegistered = () => {
     const found = user.registrations?.find(r => r.course.id === course.id);
-
     return found !== undefined;
   };
-
-  const classes = useStyles();
 
   return (
     <>
       {/* eslint-disable-next-line no-nested-ternary */}
-      {userIsRegistered() ? (
-        courseHasGroups() ? (
-          <>
-            {group[0].groupMessage && group[0].groupMessage !== '' && (
-              <Box component={Paper} width="75%" m={2} p={2} className={classes.messageBox}>
-                <Typography variant="h5" gutterBottom>
-                  <FormattedMessage id="groups.newMessage" />
-                  &nbsp;
-                  {group[0].groupName}: ({course.title})
-                </Typography>
-                <Typography variant="body1" className={classes.message}>
-                  {group[0].groupMessage}
-                </Typography>
-              </Box>
-            )}
+      {userIsRegistered() && courseHasGroups() && (
+            <>
             <UserGroupItem group={group[0]} groupTimes={groupTimes} course={course} />
-          </>
-        ) : null
-      ) : null}
+            <br></br>
+            </>
+         )}
     </>
   );
 };
