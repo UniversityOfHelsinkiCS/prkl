faa451d9717569e304b0482fddc2a763d81dc4e7
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-05-30 16:33:58 +0300

    Update ormconfig

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,18 +1,18 @@
 const devMode = process.env.NODE_ENV === "development";
 const testMode = process.env.NODE_ENV === "test";
 
-const prefix = devMode ? "src" : "dist/src";
+const prefix = devMode ? "src" : "dist";
 const ext = devMode ? "ts" : "js";
 
-const dbHost = testMode ? "test-db" : "db";
+const dbHost = testMode ? "test-db" : process.env.POSTGRES_HOST;
 
 module.exports = {
   type: "postgres",
   host: dbHost,
   port: 5432,
-  database: "postgres",
-  username: "postgres",
-  password: "postgres",
+  database: process.env.POSTGRES_DB,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
   entities: [`${prefix}/entities/*.${ext}`],
   migrationsTableName: "migrations",
   migrations: [`${prefix}/migrations/*.${ext}`],
